2123abc947cf0642b793e97bafe82d23657aa08a
Félix Dorn <git@xfe.li>
2026-01-30 22:39:10 +0100

    remove translations

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://atlas.foreview.org',
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'fr'],
-    routing: {
-      prefixDefaultLocale: false, // /read for English, /fr/read for French
-    },
-  },
   env: {
     schema: {
       GOOGLE_CREDENTIALS_BASE64: envField.string({ context: "server", access: "secret"})
