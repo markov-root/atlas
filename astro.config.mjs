df09dd912b76139d320e4be4667947d9f83a1d59
Félix Dorn <git@xfe.li>
2026-02-02 16:34:17 +0100

    set up ai-safety-atlas.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://atlas.foreview.org',
+  site: 'https://ai-safety-atlas.com',
   integrations: [icon()],
   env: {
     schema: {
